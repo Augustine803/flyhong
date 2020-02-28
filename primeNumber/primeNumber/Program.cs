ce1db371d8d4d07e0aa89fbf0b523465fe42a141
Augutine <51405214+Augutine@users.noreply.github.com>
2020-02-28 15:57:07 +0800

    Add files via upload

---
+++ primeNumber/primeNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace primeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("输入数字");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number == 1)
+            {
+                Console.WriteLine("1没有素数因子");
+            }
+            for(int i = 2; i <= number/2; i++)
+            {
+                if (number % i == 0)
+                {
+                    Console.WriteLine($"{i}");
+                    while (number % i == 0)
+                    {
+                        number /= i;
+                    }
+                }
+                if (number == 1) break;
+
+            }
+        }
+    }
+}
