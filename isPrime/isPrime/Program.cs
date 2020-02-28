b2194aeb747c830134920d3d5b70c74dca5ed781
Augutine <51405214+Augutine@users.noreply.github.com>
2020-02-28 16:24:26 +0800

    Add files via upload

---
+++ isPrime/isPrime/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace isPrime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool[] isprime = new bool[101];
+            for(int i = 0; i< isprime.Length; i++)
+            {
+                isprime[i] = true;
+            }
+            isprime[0] = false;
+            isprime[1] = false;
+            for(int i = 2; i* i<= 100; i++)
+            {
+                if(isprime[i])
+                {
+                    for(int j = i * 2; j <= 100; j=j+i)
+                    {
+                        isprime[j] = false;
+                    }
+                }
+            }
+            for(int i = 0; i < isprime.Length; i++)
+            {
+                if (isprime[i])
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
