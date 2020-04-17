9be4204c6d3169b5c0e2e996348a5456794bd46f
Augutine <51405214+Augutine@users.noreply.github.com>
2020-04-17 12:21:53 +0800

    Add files via upload

---
+++ CayleyTree/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace CayleyTree
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+      this.panel1 = new System.Windows.Forms.Panel();
+      this.cbxColor = new System.Windows.Forms.ComboBox();
+      this.txtN = new System.Windows.Forms.TextBox();
+      this.label6 = new System.Windows.Forms.Label();
+      this.txtLength = new System.Windows.Forms.TextBox();
+      this.label5 = new System.Windows.Forms.Label();
+      this.txtPer2 = new System.Windows.Forms.TextBox();
+      this.txtPer1 = new System.Windows.Forms.TextBox();
+      this.label4 = new System.Windows.Forms.Label();
+      this.txtDegree2 = new System.Windows.Forms.TextBox();
+      this.lblLen2 = new System.Windows.Forms.Label();
+      this.label3 = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      this.txtDegree1 = new System.Windows.Forms.TextBox();
+      this.label1 = new System.Windows.Forms.Label();
+      this.btnDraw = new System.Windows.Forms.Button();
+      this.panel2 = new System.Windows.Forms.Panel();
+      this.panel1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // panel1
+      // 
+      this.panel1.AutoScroll = true;
+      this.panel1.BackColor = System.Drawing.SystemColors.ButtonFace;
+      this.panel1.Controls.Add(this.cbxColor);
+      this.panel1.Controls.Add(this.txtN);
+      this.panel1.Controls.Add(this.label6);
+      this.panel1.Controls.Add(this.txtLength);
+      this.panel1.Controls.Add(this.label5);
+      this.panel1.Controls.Add(this.txtPer2);
+      this.panel1.Controls.Add(this.txtPer1);
+      this.panel1.Controls.Add(this.label4);
+      this.panel1.Controls.Add(this.txtDegree2);
+      this.panel1.Controls.Add(this.lblLen2);
+      this.panel1.Controls.Add(this.label3);
+      this.panel1.Controls.Add(this.label2);
+      this.panel1.Controls.Add(this.txtDegree1);
+      this.panel1.Controls.Add(this.label1);
+      this.panel1.Controls.Add(this.btnDraw);
+      this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+      this.panel1.Location = new System.Drawing.Point(635, 6);
+      this.panel1.Margin = new System.Windows.Forms.Padding(4);
+      this.panel1.Name = "panel1";
+      this.panel1.Size = new System.Drawing.Size(339, 472);
+      this.panel1.TabIndex = 7;
+      // 
+      // cbxColor
+      // 
+      this.cbxColor.FormattingEnabled = true;
+      this.cbxColor.Location = new System.Drawing.Point(157, 331);
+      this.cbxColor.Name = "cbxColor";
+      this.cbxColor.Size = new System.Drawing.Size(148, 23);
+      this.cbxColor.TabIndex = 23;
+      // 
+      // txtN
+      // 
+      this.txtN.Location = new System.Drawing.Point(157, 285);
+      this.txtN.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+      this.txtN.Name = "txtN";
+      this.txtN.Size = new System.Drawing.Size(148, 25);
+      this.txtN.TabIndex = 5;
+      // 
+      // label6
+      // 
+      this.label6.AutoSize = true;
+      this.label6.Location = new System.Drawing.Point(64, 290);
+      this.label6.Name = "label6";
+      this.label6.Size = new System.Drawing.Size(67, 15);
+      this.label6.TabIndex = 22;
+      this.label6.Text = "分叉次数";
+      // 
+      // txtLength
+      // 
+      this.txtLength.Location = new System.Drawing.Point(157, 238);
+      this.txtLength.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+      this.txtLength.Name = "txtLength";
+      this.txtLength.Size = new System.Drawing.Size(148, 25);
+      this.txtLength.TabIndex = 4;
+      // 
+      // label5
+      // 
+      this.label5.AutoSize = true;
+      this.label5.Location = new System.Drawing.Point(64, 242);
+      this.label5.Name = "label5";
+      this.label5.Size = new System.Drawing.Size(67, 15);
+      this.label5.TabIndex = 20;
+      this.label5.Text = "主干高度";
+      // 
+      // txtPer2
+      // 
+      this.txtPer2.Location = new System.Drawing.Point(157, 192);
+      this.txtPer2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+      this.txtPer2.Name = "txtPer2";
+      this.txtPer2.Size = new System.Drawing.Size(148, 25);
+      this.txtPer2.TabIndex = 3;
+      // 
+      // txtPer1
+      // 
+      this.txtPer1.Location = new System.Drawing.Point(157, 148);
+      this.txtPer1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+      this.txtPer1.Name = "txtPer1";
+      this.txtPer1.Size = new System.Drawing.Size(148, 25);
+      this.txtPer1.TabIndex = 2;
+      // 
+      // label4
+      // 
+      this.label4.AutoSize = true;
+      this.label4.Location = new System.Drawing.Point(88, 51);
+      this.label4.Name = "label4";
+      this.label4.Size = new System.Drawing.Size(45, 15);
+      this.label4.TabIndex = 17;
+      this.label4.Text = "角度1";
+      // 
+      // txtDegree2
+      // 
+      this.txtDegree2.Location = new System.Drawing.Point(157, 95);
+      this.txtDegree2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+      this.txtDegree2.Name = "txtDegree2";
+      this.txtDegree2.Size = new System.Drawing.Size(148, 25);
+      this.txtDegree2.TabIndex = 1;
+      // 
+      // lblLen2
+      // 
+      this.lblLen2.AutoSize = true;
+      this.lblLen2.Location = new System.Drawing.Point(40, 196);
+      this.lblLen2.Name = "lblLen2";
+      this.lblLen2.Size = new System.Drawing.Size(90, 15);
+      this.lblLen2.TabIndex = 15;
+      this.lblLen2.Text = "分支长度比2";
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Location = new System.Drawing.Point(88, 336);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(37, 15);
+      this.label3.TabIndex = 13;
+      this.label3.Text = "颜色";
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(88, 99);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(45, 15);
+      this.label2.TabIndex = 11;
+      this.label2.Text = "角度2";
+      // 
+      // txtDegree1
+      // 
+      this.txtDegree1.Location = new System.Drawing.Point(157, 48);
+      this.txtDegree1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+      this.txtDegree1.Name = "txtDegree1";
+      this.txtDegree1.Size = new System.Drawing.Size(148, 25);
+      this.txtDegree1.TabIndex = 0;
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(40, 148);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(90, 15);
+      this.label1.TabIndex = 9;
+      this.label1.Text = "分支长度比1";
+      // 
+      // btnDraw
+      // 
+      this.btnDraw.Location = new System.Drawing.Point(135, 385);
+      this.btnDraw.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+      this.btnDraw.Name = "btnDraw";
+      this.btnDraw.Size = new System.Drawing.Size(97, 39);
+      this.btnDraw.TabIndex = 7;
+      this.btnDraw.Text = "绘图";
+      this.btnDraw.UseVisualStyleBackColor = true;
+      this.btnDraw.Click += new System.EventHandler(this.btnDraw_Click);
+      // 
+      // panel2
+      // 
+      this.panel2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+      this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+      this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.panel2.Location = new System.Drawing.Point(7, 6);
+      this.panel2.Margin = new System.Windows.Forms.Padding(4);
+      this.panel2.Name = "panel2";
+      this.panel2.Size = new System.Drawing.Size(628, 472);
+      this.panel2.TabIndex = 8;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(981, 484);
+      this.Controls.Add(this.panel2);
+      this.Controls.Add(this.panel1);
+      this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+      this.Name = "Form1";
+      this.Padding = new System.Windows.Forms.Padding(7, 6, 7, 6);
+      this.Text = "Form1";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.panel1.ResumeLayout(false);
+      this.panel1.PerformLayout();
+      this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDegree1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnDraw;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtDegree2;
+        private System.Windows.Forms.Label lblLen2;
+        private System.Windows.Forms.TextBox txtPer2;
+        private System.Windows.Forms.TextBox txtPer1;
+        private System.Windows.Forms.TextBox txtLength;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtN;
+        private System.Windows.Forms.Label label6;
+    private System.Windows.Forms.ComboBox cbxColor;
+  }
+}
+
---
+++ CayleyTree/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CayleyTree {
+  public partial class Form1 : Form {
+
+    private Graphics graphics;
+    public int Degree1 { get; set; }//分支角度
+    public int Degree2 { get; set; }//分支角度
+    public double Per1 { get; set; }//分支长度比1
+    public double Per2 { get; set; }//分支长度比2
+    public double Length { get; set; } //主干高度
+    public int N { get; set; }//迭代次数
+    public Pen DrawPen { get; set; }//画笔
+
+    public Form1() {
+      InitializeComponent();
+    }
+
+    private void Form1_Load(object sender, EventArgs e) {
+      Degree1 = 30; Degree2 = 20; Per1 = 0.6; Per2 = 0.7; Length = 100; N = 10;
+      DrawPen = Pens.Red;
+      Pen[] pens = { Pens.Red, Pens.Green, Pens.Yellow };
+      cbxColor.DataSource = pens;
+      cbxColor.DisplayMember = "Color";
+      cbxColor.DataBindings.Add("SelectedItem", this, "DrawPen");
+
+      txtDegree1.DataBindings.Add("Text", this, "Degree1");
+      txtDegree2.DataBindings.Add("Text", this, "Degree2");
+      txtPer1.DataBindings.Add("Text", this, "Per1");
+      txtPer2.DataBindings.Add("Text", this, "Per2");
+      txtLength.DataBindings.Add("Text", this, "Length");
+      txtN.DataBindings.Add("Text", this, "N");
+    }
+
+    private void btnDraw_Click(object sender, EventArgs e) {
+      graphics = this.panel2.CreateGraphics();
+      graphics.Clear(panel2.BackColor);
+      drawCayleyTree(this.N, panel2.Width / 2, panel2.Height - 20, this.Length, -Math.PI / 2);
+    }
+
+    void drawCayleyTree(int n, double x0, double y0, double len, double th) {
+      if (n == 0) return;
+      double x1 = x0 + len * Math.Cos(th);
+      double y1 = y0 + len * Math.Sin(th);
+      graphics.DrawLine(DrawPen,
+         (int)x0, (int)y0, (int)x1, (int)y1);
+      drawCayleyTree(n - 1, x1, y1, this.Per1 * len, th + this.Degree1 * Math.PI / 180);
+      drawCayleyTree(n - 1, x1, y1, this.Per2 * len, th - this.Degree2 * Math.PI / 180);
+    }
+
+  }
+}
